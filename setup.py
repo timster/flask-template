f38753008f793f61964f6a01b5cb10dc60905dcd
Tim Shaffer <tshaffer@mitre.org>
2018-04-18 11:24:10 -0400

    Update build process.

--- setup.py
+++ setup.py
@@ -1,13 +1,29 @@
 import sys
-from pip.download import PipSession
-from pip.req import parse_requirements
+
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
 
-def get_requirements(filename):
-    requirements = parse_requirements(filename, session=PipSession())
-    return list(str(req.req) for req in requirements)
+requirements = [
+    'flask==0.11.1',
+    'flask-cors==3.0.2',
+    'flask-mail==0.9.1',
+    'flask-httpauth==3.2.1',
+    'peewee==2.8.5',
+    'peewee-moves==1.6.1',
+    'peewee-validates==0.5.0',
+    'markdown==2.6.7',
+]
+
+dev_requirements = [
+    'sphinx==1.7.2',
+]
+
+test_requirements = [
+    'pytest==3.0.5',
+    'pytest-cov==2.4.0',
+    'pytest-mock==1.5.0',
+]
 
 
 class PyTest(TestCommand):
@@ -27,8 +43,9 @@ setup(
     author='Tim Shaffer',
     version='1.0.0',
     include_package_data=True,
-    install_requires=get_requirements('requirements.txt'),
-    tests_require=get_requirements('requirements-test.txt'),
+    install_requires=requirements,
+    tests_require=test_requirements,
+    extras_require={'test': test_requirements, 'dev': dev_requirements},
     test_suite='tests',
     cmdclass={'test': PyTest},
 )
